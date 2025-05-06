00ff7d77e38557db26384530a22640c45c3f3d54
Swayam <158766919+Swayamjimmy@users.noreply.github.com>
2025-05-06 17:01:32 +0530

    Added buttons for ui

--- rps.js
+++ rps.js
@@ -2,76 +2,25 @@ function getComputerChoice() {
     let a = Math.random()
     if (a < 0.33) return "ROCK"
     if (a < 0.66) return "PAPER"
-    if (a > 0.66) return "SCISSORS" 
+    if (a >= 0.66) return "SCISSORS" 
     
 }
-
-function getHumanChoice() {
-    let a = prompt("rock paper or scissors?")
-    return a.toUpperCase()
-}
-
-
 
 function playRound(human , comp, humanScore, compScore) {
 
   if ((human == "ROCK" && comp == "PAPER")|| (human == "PAPER" && comp == "SCISSORS")||( human == "SCISSORS" && comp == "ROCK")) {
-  console.log("You Lose")
-  return 0 ; }
-  else if ((human == "ROCK" && comp == "SCISSORS")|| (human == "PAPER" && comp == "ROCK")|| (human == "SCISSORS" && comp == "PAPER")) { console.log("You Win") 
-  return 1; }
+  console.log("You Lose") }
+  else if ((human == "ROCK" && comp == "SCISSORS")|| (human == "PAPER" && comp == "ROCK")|| (human == "SCISSORS" && comp == "PAPER")) { console.log("You Win") }
   else if (human == comp) console.log("It was a draw, another round");
   else console.log("debug");
 
 }
-
-function playGame() {
-    let humanScore = 0
-    let compScore = 0
-
-let human = getHumanChoice()
-console.log(`You chose ${human}`)
-let comp = getComputerChoice()
-let a = playRound(human, comp, humanScore, compScore)
-console.log(`Computer chose ${comp}`)
-if (a == 0) compScore++;
-else if (a == 1) humanScore++;
-
-human = getHumanChoice()
-console.log(`You chose ${human}`)
-comp = getComputerChoice()
-a = playRound(human, comp, humanScore, compScore)
-console.log(`Computer chose ${comp}`)
-if (a == 0) compScore++;
-else if (a == 1) humanScore++;
-
-human = getHumanChoice()
-console.log(`You chose ${human}`)
-comp = getComputerChoice()
-a = playRound(human, comp, humanScore, compScore)
-console.log(`Computer chose ${comp}`)
-if (a == 0) compScore++;
-else if (a == 1) humanScore++;
-
-human = getHumanChoice()
-console.log(`You chose ${human}`)
-comp = getComputerChoice()
-a = playRound(human, comp, humanScore, compScore)
-console.log(`Computer chose ${comp}`)
-if (a == 0) compScore++;
-else if (a == 1)humanScore++;
-
-human = getHumanChoice()
-console.log(`You chose ${human}`)
-comp = getComputerChoice()
-a = playRound(human, comp, humanScore, compScore)
-console.log(`Computer chose ${comp}`)
-if (a == 0) compScore++;
-else if (a == 1) humanScore++;
-
-console.log(humanScore)
-console.log(compScore)
-
-}
-
-playGame()
+  let check = document.querySelectorAll('#btn');
+  check.forEach((button) => {
+    button.addEventListener('click', () => {
+    console.log(button.textContent);
+    let humanChoice = button.textContent;
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    })
+  })
